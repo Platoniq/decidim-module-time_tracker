9b8c238b1df83d82c0139b766d518b4f1e772159
David Igón <davidbenabarre@platoniq.net>
2020-03-23 18:53:26 +0100

    fix redirect_to in activities controller to use EngineRouter

--- app/controllers/decidim/time_tracker/admin/activities_controller.rb
+++ app/controllers/decidim/time_tracker/admin/activities_controller.rb
@@ -11,7 +11,7 @@ module Decidim
         end
 
         def new
-          enforce_permission_to :create, :task
+          enforce_permission_to :create, :activities
 
           @form = form(TaskActivityForm).instance
         end
@@ -22,14 +22,14 @@ module Decidim
         end
 
         def create
-          enforce_permission_to :create, :task
+          enforce_permission_to :create, :activities
 
           @form = form(TaskActivityForm).from_params(params)
 
           CreateActivity.call(@form, current_task) do
             on(:ok) do
               flash[:notice] = I18n.t("activities.create.success", scope: "decidim.time_tracker.admin")
-              redirect_to task_activities_path
+              redirect_to EngineRouter.admin_proxy(current_component).task_activities_path(current_task)
             end
 
             on(:invalid) do
@@ -42,11 +42,10 @@ module Decidim
         def update
           enforce_permission_to :update, :activity, activity: current_activity
           form = form(TaskActivityForm).from_params(params, task: current_task, activity: current_activity)
-
           UpdateActivity.call(current_activity, form, current_user) do
             on(:ok) do
               flash[:notice] = I18n.t("activities.update.success", scope: "decidim.time_tracker.admin")
-              redirect_to task_activities_path
+              redirect_to EngineRouter.admin_proxy(current_component).task_activities_path(current_task)
             end
 
             on(:invalid) do
@@ -62,7 +61,7 @@ module Decidim
           DestroyActivity.call(current_activity, current_user) do
             on(:ok) do
               flash[:notice] = I18n.t("activities.destroy.success", scope: "decidim.time_tracker.admin")
-              redirect_to task_activities_path
+              redirect_to EngineRouter.admin_proxy(current_component).task_activities_path(current_task)
             end
           end
         end
