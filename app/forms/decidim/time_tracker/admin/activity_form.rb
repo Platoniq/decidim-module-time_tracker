a0670c814210a8ed988ab494697c31d9d6e8a205
Ivan Vergés <ivan@platoniq.net>
2020-04-07 20:53:11 +0200

    add forms tests

--- app/forms/decidim/time_tracker/admin/activity_form.rb
+++ app/forms/decidim/time_tracker/admin/activity_form.rb
@@ -8,14 +8,16 @@ module Decidim
         include TranslatableAttributes
 
         translatable_attribute :description, String
+
         attribute :active, Boolean
         attribute :start_date, Decidim::Attributes::LocalizedDate
         attribute :end_date, Decidim::Attributes::LocalizedDate
         attribute :max_minutes_per_day, Integer
         attribute :requests_start_at, Decidim::Attributes::TimeWithZone
 
-        validates :description, translatable_presence: true
         validates :max_minutes_per_day, presence: true
+        validates :description, translatable_presence: true
+        # TODO: validate end_date after start_date if not empty
       end
     end
   end
---
+++ spec/forms/admin/task_form_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim::TimeTracker
+  describe Admin::TaskForm do
+    subject(:form) { described_class.from_params(attributes).with_context(context) }
+
+    let(:organization) { create(:organization, available_locales: [:en]) }
+    let(:context) do
+      {
+        current_organization: organization,
+        current_component: current_component,
+        current_participatory_space: participatory_process
+      }
+    end
+    let(:participatory_process) { create :participatory_process, organization: organization }
+    let(:current_component) { create :time_tracker_component, participatory_space: participatory_process }
+
+    let(:name) do
+      Decidim::Faker::Localized.sentence(3)
+    end
+
+    let(:attributes) do
+      {
+        name: name
+      }
+    end
+
+    it { is_expected.to be_valid }
+  end
+end
