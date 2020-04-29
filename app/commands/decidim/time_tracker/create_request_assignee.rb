9f67978ec7eee31730ef33b0a9ae00b5b73f50c6
David Igón <davidbenabarre@platoniq.net>
2020-04-29 18:58:29 +0200

    add new commands for handling with time entries and requests to be assignees

---
+++ app/commands/decidim/time_tracker/create_request_assignee.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Decidim
+  module TimeTracker
+    # A command with all the business logic when requesting to be an assignee
+    class CreateRequestAssignee < Rectify::Command
+      def initialize(activity, user)
+        @activity = activity
+        @user = user
+      end
+
+      # Creates the meeting if valid.
+      #
+      # Broadcasts :ok if successful, :invalid otherwise.
+      def call
+        # return broadcast(:invalid)
+
+        create_request_assignee
+
+        broadcast(:ok)
+      end
+
+      def create_request_assignee
+        Decidim.traceability.create!(
+          Decidim::TimeTracker::Assignee,
+          @user,
+          activity: @activity,
+          user: @user,
+          status: :pending,
+          requested_at: Time.current
+        )
+      end
+    end
+  end
+end
