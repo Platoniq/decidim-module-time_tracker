337be789361d3c7ee4dbab41ce33be0e0af90f17
David Igón <davidbenabarre@platoniq.net>
2020-05-11 17:00:43 +0200

    add helper method to check if assignee is accepted in time tracker controller

--- app/controllers/decidim/time_tracker/time_tracker_controller.rb
+++ app/controllers/decidim/time_tracker/time_tracker_controller.rb
@@ -3,7 +3,7 @@
 module Decidim
   module TimeTracker
     class TimeTrackerController < Decidim::TimeTracker::ApplicationController
-      helper_method :tasks, :last_time_entry
+      helper_method :tasks, :last_time_entry, :assignee_accepted
 
       private
 
@@ -14,6 +14,11 @@ module Decidim
       def last_time_entry(assignee)
         time_entry = Decidim::TimeTracker::TimeEntry.where(assignee: assignee).last
         return time_entry if !time_entry.nil? && time_entry.time_end.nil?
+      end
+
+      def assignee_accepted(activity, current_user)
+        assignee = Decidim::TimeTracker::Assignee.find_by(activity: activity, user: current_user)
+        assignee.status == "accepted"
       end
     end
   end
