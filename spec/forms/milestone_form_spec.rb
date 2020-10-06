14908e29fe8acdbf5d09c9c124b87be6f0a76aa3
Ivan Vergés <ivan@platoniq.net>
2020-10-06 11:10:20 +0200

    fix milestone form spec

--- spec/forms/milestone_form_spec.rb
+++ spec/forms/milestone_form_spec.rb
@@ -4,40 +4,45 @@ require "spec_helper"
 
 module Decidim::TimeTracker
   describe MilestoneForm do
-    subject do
-      described_class.new(
+    subject(:form) { described_class.from_params(attributes) }
+
+    let(:activity) { create :activity }
+    let(:title) { "My milestone" }
+    let(:attachment_params) { nil }
+    let(:attributes) do
+      {
+        activity_id: activity.id,
         title: title,
-        file: file
-      )
+        attachment: attachment_params
+      }
     end
 
-    let(:title) { "My attachment" }
-    let(:file) { Decidim::Dev.test_file("city.jpeg", "image/jpeg") }
+    it { is_expected.to be_valid }
 
-    context "with correct data" do
-      it "is valid" do
-        expect(subject).to be_valid
+    describe "when title is missing" do
+      let(:title) { nil }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context "when the attachment is present" do
+      let(:attachment_params) do
+        {
+          title: "My attachment",
+          file: Decidim::Dev.test_file("city.jpeg", "image/jpeg")
+        }
       end
-    end
 
-    context "when the file is present" do
-      context "and the title is not present" do
-        let(:title) { "" }
+      it { is_expected.to be_valid }
 
-        it "is not valid" do
+      context "when the form has some errors" do
+        let(:title) { nil }
+
+        it "adds an error to the `:attachment` field" do
           expect(subject).not_to be_valid
-        end
-      end
-    end
 
-    context "when the file is not present" do
-      let(:file) { nil }
-
-      context "and the title is not present" do
-        let(:title) { "" }
-
-        it "is not valid" do
-          expect(subject).to be_valid
+          expect(subject.errors.full_messages).to match_array(["Attachment Needs to be reattached", "What have you done? can't be blank"])
+          expect(subject.errors.keys).to match_array([:title, :attachment])
         end
       end
     end
