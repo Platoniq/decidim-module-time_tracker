3424ca65d4c9091d249486eb31b11c5f8279c691
Vera Rojman <vrojman@protonmail.com>
2020-10-28 10:50:46 +0100

    Add info to tasks index view

--- app/models/decidim/time_tracker/task.rb
+++ app/models/decidim/time_tracker/task.rb
@@ -13,6 +13,20 @@ module Decidim
       has_many :activities,
                class_name: "Decidim::TimeTracker::Activity",
                dependent: :destroy
+      
+      def starts_at
+        activities.order(start_date: :asc).first&.start_date
+      end
+      
+      def ends_at
+        activities.order(end_date: :desc).first&.end_date
+      end
+
+      def assignees_count(filter: :accepted)
+        assignees = Assignee.where(activity: activities)
+        assignees = assignees.send(filter) if [:pending, :accepted, :rejected].include? filter
+        assignees.count
+      end
     end
   end
 end
