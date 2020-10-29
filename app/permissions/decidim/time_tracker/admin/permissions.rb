4db073e289dbe5d00afcd6e2c277c4a662354a6c
Vera Rojman <vrojman@protonmail.com>
2020-10-29 16:07:30 +0100

    Add constraint for updating assignee

--- app/permissions/decidim/time_tracker/admin/permissions.rb
+++ app/permissions/decidim/time_tracker/admin/permissions.rb
@@ -7,9 +7,8 @@ module Decidim
         def permissions
           return permission_action if permission_action.scope != :admin
 
-          permission_action.allow! if permission_action.action.in? [:update, :destroy]
-
           allowed_task_action?
+          allowed_assignee_action?
 
           permission_action
         end
@@ -21,6 +20,23 @@ module Decidim
           when :create, :update, :destroy
             permission_action.allow!
           end
+        end
+
+        def allowed_assignee_action?
+          return unless permission_action.subject == :assignee
+          
+          if permission_action.action == :update
+            return permission_action.allow! if assignee.can_change_status?
+          end
+
+          case permission_action.action
+          when :create, :destroy
+            permission_action.allow!
+          end
+        end
+
+        def assignee
+          @assignee ||= context.fetch(:assignee, nil)
         end
       end
     end
