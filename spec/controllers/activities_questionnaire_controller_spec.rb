3f6a9fbb3174f7f9478951e83bbfca013670ae57
Ivan Vergés <ivan@platoniq.net>
2020-11-02 20:53:43 +0100

    Compatibility with > 0.22 (#42)
    * add answer options endpoint for survey logic compatibility
    
    * add testing cases
    
    * add answering questionnare spec

--- spec/controllers/activities_questionnaire_controller_spec.rb
+++ spec/controllers/activities_questionnaire_controller_spec.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "decidim/forms/test/factories"
 
 module Decidim::TimeTracker
   describe ActivitiesQuestionnaireController, type: :controller do
     routes { Decidim::TimeTracker::Engine.routes }
 
-    let(:organization) { create :organization }
-    let(:user) { create(:user, :confirmed, :admin, organization: organization) }
-    let(:participatory_space) { create(:participatory_process, organization: organization) }
+    let!(:user) { create(:user, :confirmed, organization: organization) }
+    let(:organization) { participatory_space.organization }
+    let(:participatory_space) { activity.task.component.participatory_space }
     let(:component) { activity.task.component }
-    let(:assignee) { create :assignee, activity: activity, user: user }
-    let!(:activity) { create :activity }
+    let(:questionnaire) { create :questionnaire }
+    let(:task) { create :task, questionnaire: questionnaire }
+    let!(:activity) { create :activity, task: task }
 
     let(:form) do
       {
@@ -26,19 +28,117 @@ module Decidim::TimeTracker
       sign_in user
     end
 
+    shared_examples "renders the form readonly" do |can_answer|
+      it "do not allow answers" do
+        get :show, params: params
+
+        expect(response).to have_http_status(:ok)
+        expect(controller.helpers.questionnaire_for).to eq(activity.task)
+        expect(controller.helpers.allow_answers?).to eq(false)
+        expect(controller.helpers.visitor_can_answer?).to eq(can_answer)
+        expect(controller.helpers.visitor_already_answered?).not_to eq(true)
+        expect(subject).to render_template(:show)
+      end
+    end
+
+    shared_examples "renders the form" do
+      it "allows answers" do
+        get :show, params: params
+
+        expect(response).to have_http_status(:ok)
+        expect(controller.helpers.questionnaire_for).to eq(activity.task)
+        expect(controller.helpers.allow_answers?).to eq(true)
+        expect(controller.helpers.visitor_can_answer?).to eq(true)
+        expect(controller.helpers.visitor_already_answered?).to eq(false)
+        expect(subject).to render_template(:show)
+      end
+    end
+
     describe "GET #show" do
       let(:params) do
         {
           task_id: activity.task.id,
           activity_id: activity.id,
-          id: activity.questionnaire.id
+          id: questionnaire.id
         }
       end
 
-      it "renders the form" do
-        get :show, params: params
-        expect(response).to have_http_status(:ok)
-        expect(subject).to render_template(:show)
+      context "when user is not logged" do
+        it_behaves_like "renders the form readonly", false
+      end
+
+      context "when user is logged" do
+        before do
+          sign_in user
+        end
+
+        context "and user is not an assignee" do
+          it_behaves_like "renders the form readonly", false
+        end
+
+        context "and user is an assignee" do
+          let!(:assignee) { create :assignee, activity: activity, user: user }
+
+          context "and questionnaire do not have questions" do
+            it_behaves_like "renders the form readonly", true
+          end
+
+          context "and questionnaire have questions" do
+            let(:questionnaire) { create :questionnaire, :with_questions }
+
+            it_behaves_like "renders the form"
+          end
+        end
+      end
+    end
+
+    describe "POST #answer" do
+      let!(:question) { create :questionnaire_question, question_type: :short_answer, body: "name", questionnaire: questionnaire }
+      let(:tos_agreement) { "0" }
+      let(:form) do
+        {
+          tos_agreement: tos_agreement,
+          responses: {
+            "0" => {
+              body: "Tom",
+              question_id: question.id
+            }
+          }
+        }
+      end
+      let(:params) do
+        {
+          task_id: task.id,
+          activity_id: activity.id,
+          id: questionnaire.id,
+          questionnaire: form
+        }
+      end
+
+      before do
+        sign_in user
+      end
+
+      context "and questionnaire is not valid" do
+        it "do not update the form" do
+          post :answer, params: params
+
+          expect(flash[:alert]).to be_present
+          expect(questionnaire).not_to be_answered_by(user)
+          expect(response).to render_template(:show)
+        end
+      end
+
+      context "and questionnaire is valid" do
+        let(:tos_agreement) { "1" }
+
+        it "updates the form" do
+          post :answer, params: params
+
+          expect(flash[:notice]).to be_present
+          expect(questionnaire).to be_answered_by(user)
+          expect(response).to have_http_status(:redirect)
+        end
       end
     end
   end
