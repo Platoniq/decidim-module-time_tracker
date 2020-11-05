52e6a42df9f03df287c3226eae835022d375249c
Vera Rojman <vrojman@protonmail.com>
2020-11-05 14:09:19 +0100

    Fix rubocop offenses

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -8,20 +8,20 @@ module Decidim
       describe "clockify_seconds" do
         it "renders the time as hours, minutes and seconds" do
           expect(helper.clockify_seconds(1337)).to match(
-            "<span class=\"time-tracker--clock\">" +
-              "<span class=\"text-muted\">0h</span>" +
-              "<span>22m</span>" +
-              "<span>17s</span>" +
+            "<span class=\"time-tracker--clock\">" \
+              "<span class=\"text-muted\">0h</span>" \
+              "<span>22m</span>" \
+              "<span>17s</span>" \
             "</span>"
           )
         end
-        
+
         it "renders hours, minutes and seconds with \"text-muted\" class when their value is zero" do
           expect(helper.clockify_seconds(0)).to match(
-            "<span class=\"time-tracker--clock\">" +
-              "<span class=\"text-muted\">0h</span>" +
-              "<span class=\"text-muted\">0m</span>" +
-              "<span class=\"text-muted\">0s</span>" +
+            "<span class=\"time-tracker--clock\">" \
+              "<span class=\"text-muted\">0h</span>" \
+              "<span class=\"text-muted\">0m</span>" \
+              "<span class=\"text-muted\">0s</span>" \
             "</span>"
           )
         end
@@ -29,10 +29,10 @@ module Decidim
         context "when :padded" do
           it "renders the time as hours, minutes and seconds with two digits" do
             expect(helper.clockify_seconds(2, padded: true)).to match(
-              "<span class=\"time-tracker--clock\">" +
-                "<span class=\"text-muted\">00h</span>" +
-                "<span class=\"text-muted\">00m</span>" +
-                "<span>02s</span>" +
+              "<span class=\"time-tracker--clock\">" \
+                "<span class=\"text-muted\">00h</span>" \
+                "<span class=\"text-muted\">00m</span>" \
+                "<span>02s</span>" \
               "</span>"
             )
           end
