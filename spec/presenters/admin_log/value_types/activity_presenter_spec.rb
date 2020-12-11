944977e6a082cfd69845a33ead548cbaf3836bd2
Ivan Vergés <ivan@platoniq.net>
2020-12-11 08:53:53 +0100

    add value types specs

---
+++ spec/presenters/admin_log/value_types/activity_presenter_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim::TimeTracker
+  describe AdminLog::ValueTypes::ActivityPresenter do
+    include Decidim::TranslatableAttributes
+
+    subject { described_class.new(activity_id, self) }
+
+    let(:activity) { create :activity, description: description }
+    let(:activity_id) { activity.id }
+    let(:description) do
+      {
+        "en" => "My value",
+        "es" => "My title in Spanish"
+      }
+    end
+
+    describe "#present" do
+      it "handles i18n fields" do
+        expect(subject.present).to eq "My value"
+      end
+
+      context "when no value found" do
+        let(:activity_id) { 1234 }
+
+        it "returns not found" do
+          expect(subject.present).to include "not found"
+          expect(subject.present).to include "ID: #{activity_id}"
+        end
+      end
+    end
+  end
+end
---
+++ spec/presenters/admin_log/value_types/time_tracker_presenter_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim::TimeTracker
+  describe AdminLog::ValueTypes::TimeTrackerPresenter do
+    include Decidim::TranslatableAttributes
+
+    subject { described_class.new(time_tracker_id, self) }
+
+    let(:time_tracker) { create :time_tracker }
+    let(:time_tracker_id) { time_tracker.id }
+
+    describe "#present" do
+      it "handles i18n fields" do
+        expect(subject.present).to eq time_tracker.component.name["en"]
+      end
+
+      context "when no value found" do
+        let(:time_tracker_id) { 1234 }
+
+        it "returns not found" do
+          expect(subject.present).to include "not found"
+          expect(subject.present).to include "ID: #{time_tracker_id}"
+        end
+      end
+    end
+  end
+end
