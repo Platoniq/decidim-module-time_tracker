becfecebfc4387eff2ec4e8322be760bd04b6e46
Vera Rojman <vera@platoniq.net>
2021-01-19 16:03:31 +0100

    Add seeds for assignee questionnaire (#64)
    * Add seeds for assignee data questionnaire
    
    * Fix rubocop offenses
    
    * Normalize locales
    
    * Update readme with instructions for assignee questionnaire customization

--- spec/commands/admin/create_time_tracker_spec.rb
+++ spec/commands/admin/create_time_tracker_spec.rb
@@ -11,17 +11,6 @@ module Decidim::TimeTracker::Admin
     let(:participatory_process) { create :participatory_process, organization: organization }
     let(:component) { create :time_tracker_component, participatory_space: participatory_process }
 
-    default_questionnaire_seeds = Decidim::TimeTracker.default_questionnaire_seeds
-    custom_questionnaire_seeds = {
-      tos: "TOS",
-      title: "A questionnaire",
-      description: { en: "This is a questionnaire" },
-      questions: [
-        { question_type: "short_answer", body: "Question 1" },
-        { question_type: "single_option", body: { en: "Question 2" }, answer_options: [{ body: "Answer Option 1", free_text: true }] }
-      ]
-    }
-
     context "when the save command is not successful" do
       let(:component) { nil }
 
@@ -44,55 +33,132 @@ module Decidim::TimeTracker::Admin
       end
     end
 
-    context "when time_tracker_questionnaire_seeds config is the default" do
-      before do
-        Decidim::TimeTracker.default_questionnaire_seeds = default_questionnaire_seeds
-        subject.call
+    describe "activity questionnaire seeds" do
+      default_activity_questionnaire_seeds = Decidim::TimeTracker.default_activity_questionnaire_seeds
+      custom_activity_questionnaire_seeds = {
+        tos: "TOS",
+        title: "A questionnaire",
+        description: { en: "This is a questionnaire" },
+        questions: [
+          { question_type: "short_answer", body: "Question 1" },
+          { question_type: "single_option", body: { en: "Question 2" }, answer_options: [{ body: "Answer Option 1", free_text: true }] }
+        ]
+      }
+
+      context "when time_tracker_questionnaire_seeds config is the default" do
+        before do
+          Decidim::TimeTracker.default_activity_questionnaire_seeds = default_activity_questionnaire_seeds
+          subject.call
+        end
+
+        it "has questions" do
+          expect(subject.time_tracker.has_questions?).to be true
+          expect(subject.questionnaire.title["en"]).to eq "How do you perceive this task?"
+          expect(subject.questionnaire.questions.count).to eq 3
+          expect(subject.questionnaire.questions.first.question_type).to eq "single_option"
+          expect(subject.questionnaire.questions.first.position).to eq 1
+          expect(subject.questionnaire.questions.first.body["en"]).to eq "How important do you think this task is?"
+          expect(subject.questionnaire.questions.second.question_type).to eq "separator"
+          expect(subject.questionnaire.questions.second.position).to eq 2
+          expect(subject.questionnaire.questions.second.body).to eq nil
+          expect(subject.questionnaire.questions.third.question_type).to eq "single_option"
+          expect(subject.questionnaire.questions.third.position).to eq 3
+          expect(subject.questionnaire.questions.third.body["en"]).to eq "Who do you think usually perform this task?"
+        end
       end
 
-      it "has questions" do
-        expect(subject.time_tracker.has_questions?).to be true
-        expect(subject.questionnaire.title["en"]).to eq "How do you perceive this task?"
-        expect(subject.questionnaire.questions.count).to eq 3
-        expect(subject.questionnaire.questions.first.question_type).to eq "single_option"
-        expect(subject.questionnaire.questions.first.position).to eq 1
-        expect(subject.questionnaire.questions.first.body["en"]).to eq "How important do you think this task is?"
-        expect(subject.questionnaire.questions.second.question_type).to eq "separator"
-        expect(subject.questionnaire.questions.second.position).to eq 2
-        expect(subject.questionnaire.questions.second.body).to eq nil
-        expect(subject.questionnaire.questions.third.question_type).to eq "single_option"
-        expect(subject.questionnaire.questions.third.position).to eq 3
-        expect(subject.questionnaire.questions.third.body["en"]).to eq "Who do you think usually perform this task?"
+      context "when time_tracker_questionnaire_seeds config is nil" do
+        before do
+          Decidim::TimeTracker.default_activity_questionnaire_seeds = nil
+          subject.call
+        end
+
+        it "has no questions" do
+          expect(subject.time_tracker.has_questions?).to be false
+          expect(subject.questionnaire.title).to eq nil
+          expect(subject.questionnaire.questions.count).to eq 0
+        end
+      end
+
+      context "when time_tracker_questionnaire_seeds config is customized" do
+        before do
+          Decidim::TimeTracker.default_activity_questionnaire_seeds = custom_activity_questionnaire_seeds
+          subject.call
+        end
+
+        it "has questions" do
+          expect(subject.time_tracker.has_questions?).to be true
+          expect(subject.questionnaire.title["en"]).to eq(custom_activity_questionnaire_seeds[:title])
+          expect(subject.questionnaire.tos["en"]).to eq(custom_activity_questionnaire_seeds[:tos])
+          expect(subject.questionnaire.description["en"]).to eq(custom_activity_questionnaire_seeds[:description][:en])
+          expect(subject.questionnaire.questions.first.body["en"]).to eq(custom_activity_questionnaire_seeds[:questions][0][:body])
+          expect(subject.questionnaire.questions.second.answer_options.first.body["en"]).to eq(custom_activity_questionnaire_seeds[:questions][1][:answer_options][0][:body])
+          expect(subject.questionnaire.questions.second.answer_options.first.free_text).to eq true
+        end
       end
     end
 
-    context "when time_tracker_questionnaire_seeds config is nil" do
-      before do
-        Decidim::TimeTracker.default_questionnaire_seeds = nil
-        subject.call
+    describe "assignee questionnaire seeds" do
+      default_assignee_questionnaire_seeds = Decidim::TimeTracker.default_assignee_questionnaire_seeds
+      custom_assignee_questionnaire_seeds = {
+        tos: "TOS",
+        title: "A questionnaire",
+        description: { en: "This is a questionnaire" },
+        questions: [
+          { question_type: "short_answer", body: "Question 1" },
+          { question_type: "single_option", body: { en: "Question 2" }, answer_options: [{ body: "Answer Option 1", free_text: true }] }
+        ]
+      }
+      context "when time_tracker_questionnaire_seeds config is the default" do
+        before do
+          Decidim::TimeTracker.default_assignee_questionnaire_seeds = default_assignee_questionnaire_seeds
+          subject.call
+        end
+
+        it "has assignee questions" do
+          expect(subject.time_tracker.has_assignee_questions?).to be true
+          expect(subject.assignee_questionnaire.title["en"]).to eq "Terms of use and demographic data"
+          expect(subject.assignee_questionnaire.questions.count).to eq 3
+          expect(subject.assignee_questionnaire.questions.first.question_type).to eq "single_option"
+          expect(subject.assignee_questionnaire.questions.first.position).to eq 1
+          expect(subject.assignee_questionnaire.questions.first.body["en"]).to eq "Which gender do you identify with?"
+          expect(subject.assignee_questionnaire.questions.second.question_type).to eq "separator"
+          expect(subject.assignee_questionnaire.questions.second.position).to eq 2
+          expect(subject.assignee_questionnaire.questions.second.body).to eq nil
+          expect(subject.assignee_questionnaire.questions.third.question_type).to eq "single_option"
+          expect(subject.assignee_questionnaire.questions.third.position).to eq 3
+          expect(subject.assignee_questionnaire.questions.third.body["en"]).to eq "What is your age?"
+        end
       end
 
-      it "has no questions" do
-        expect(subject.time_tracker.has_questions?).to be false
-        expect(subject.questionnaire.title).to eq nil
-        expect(subject.questionnaire.questions.count).to eq 0
-      end
-    end
+      context "when time_tracker_questionnaire_seeds config is nil" do
+        before do
+          Decidim::TimeTracker.default_assignee_questionnaire_seeds = nil
+          subject.call
+        end
 
-    context "when time_tracker_questionnaire_seeds config is customized" do
-      before do
-        Decidim::TimeTracker.default_questionnaire_seeds = custom_questionnaire_seeds
-        subject.call
+        it "has no assignee questions" do
+          expect(subject.time_tracker.has_assignee_questions?).to be false
+          expect(subject.assignee_questionnaire.title).to eq nil
+          expect(subject.assignee_questionnaire.questions.count).to eq 0
+        end
       end
 
-      it "has questions" do
-        expect(subject.time_tracker.has_questions?).to be true
-        expect(subject.questionnaire.title["en"]).to eq(custom_questionnaire_seeds[:title])
-        expect(subject.questionnaire.tos["en"]).to eq(custom_questionnaire_seeds[:tos])
-        expect(subject.questionnaire.description["en"]).to eq(custom_questionnaire_seeds[:description][:en])
-        expect(subject.questionnaire.questions.first.body["en"]).to eq(custom_questionnaire_seeds[:questions][0][:body])
-        expect(subject.questionnaire.questions.second.answer_options.first.body["en"]).to eq(custom_questionnaire_seeds[:questions][1][:answer_options][0][:body])
-        expect(subject.questionnaire.questions.second.answer_options.first.free_text).to eq true
+      context "when time_tracker_questionnaire_seeds config is customized" do
+        before do
+          Decidim::TimeTracker.default_assignee_questionnaire_seeds = custom_assignee_questionnaire_seeds
+          subject.call
+        end
+
+        it "has assignee questions" do
+          expect(subject.time_tracker.has_assignee_questions?).to be true
+          expect(subject.assignee_questionnaire.title["en"]).to eq(custom_assignee_questionnaire_seeds[:title])
+          expect(subject.assignee_questionnaire.tos["en"]).to eq(custom_assignee_questionnaire_seeds[:tos])
+          expect(subject.assignee_questionnaire.description["en"]).to eq(custom_assignee_questionnaire_seeds[:description][:en])
+          expect(subject.assignee_questionnaire.questions.first.body["en"]).to eq(custom_assignee_questionnaire_seeds[:questions][0][:body])
+          expect(subject.assignee_questionnaire.questions.second.answer_options.first.body["en"]).to eq(custom_assignee_questionnaire_seeds[:questions][1][:answer_options][0][:body])
+          expect(subject.assignee_questionnaire.questions.second.answer_options.first.free_text).to eq true
+        end
       end
     end
   end
