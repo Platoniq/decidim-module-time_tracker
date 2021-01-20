7abd6efff4388e9990580da2257caea93371c06f
Vera Rojman <vrojman@protonmail.com>
2021-01-20 13:40:41 +0100

    Separate specs for timer

--- spec/system/time_tracker_spec.rb
+++ spec/system/time_tracker_spec.rb
@@ -171,57 +171,6 @@ describe "Time tracker page", type: :system do
                   expect(page).to have_content "0h0m0s"
                 end
               end
-
-              describe "user logs time for activity" do
-                before do
-                  page.find(".time-tracker-activity-start").click
-                  sleep 1
-                end
-
-                it "starts the timer" do
-                  expect(page).not_to have_selector ".time-tracker-activity-start"
-                  expect(page).to have_selector ".time-tracker-activity-stop"
-                  expect(page).to have_selector ".time-tracker-activity-pause"
-                end
-
-                it "pauses the timer" do
-                  expect(page).not_to have_content "0h0m0s"
-                  page.find(".time-tracker-activity-pause").click
-                  expect(page).not_to have_selector ".time-tracker-activity-pause"
-                  expect(page).to have_selector ".time-tracker-activity-stop"
-                  expect(page).to have_selector ".time-tracker-activity-start"
-                end
-
-                context "when stopping the timer" do
-                  before do
-                    page.find(".time-tracker-activity-stop").click
-                  end
-
-                  it "stops the timer" do
-                    expect(page).not_to have_selector ".time-tracker-activity-stop"
-                    expect(page).not_to have_selector ".time-tracker-activity-pause"
-                    expect(page).to have_selector ".time-tracker-activity-start"
-                    expect(page).to have_content "Leave your mark"
-
-                    within ".milestone" do
-                      expect(page).to have_button "Save"
-                    end
-                  end
-
-                  it "allows submitting a milestone", processing_uploads_for: Decidim::AttachmentUploader do
-                    within ".new_milestone" do
-                      fill_in "What have you done?", with: "I saved the world from chaos!"
-                      fill_in "Description", with: "Specs are the only thing that separate us from barbaric coding"
-                      attach_file :milestone_attachment_file, Decidim::Dev.asset("city.jpeg")
-                      click_button "Save"
-                    end
-
-                    expect(page).to have_current_path milestones_path(nickname: user.nickname)
-                    expect(page).to have_content "I saved the world"
-                    expect(page).to have_link "Back to activities", href: time_tracker_path
-                  end
-                end
-              end
             end
           end
         end
@@ -229,19 +178,15 @@ describe "Time tracker page", type: :system do
     end
   end
 
-  def component_engine
-    Decidim::EngineRouter.main_proxy(component)
-  end
-
   def visit_time_tracker
     visit time_tracker_path
   end
 
   def time_tracker_path
-    component_engine.root_path
+    Decidim::EngineRouter.main_proxy(component).root_path
   end
 
-  delegate :milestones_path, to: :component_engine
-
-  delegate :assignee_questionnaire_path, to: :component_engine
+  def assignee_questionnaire_path
+    Decidim::EngineRouter.main_proxy(component).assignee_questionnaire_path
+  end
 end
