a17344c9fd2c3a14b3a2f47f8a70fddbc4a88ad5
Vera Rojman <vrojman@protonmail.com>
2021-01-21 13:30:59 +0100

    Add HasQuestionnaireAnswers concern to assignee questionnaire ctrl

--- app/controllers/decidim/time_tracker/admin/assignee_questionnaire_controller.rb
+++ app/controllers/decidim/time_tracker/admin/assignee_questionnaire_controller.rb
@@ -5,13 +5,18 @@ module Decidim
     module Admin
       class AssigneeQuestionnaireController < Admin::ApplicationController
         include Decidim::Forms::Admin::Concerns::HasQuestionnaire
-
+        include Decidim::Forms::Admin::Concerns::HasQuestionnaireAnswers
+ 
         def questionnaire_for
           time_tracker.assignee_data
         end
 
         def update_url
           EngineRouter.admin_proxy(current_component).assignee_questionnaire_path
+        end
+
+        def public_url
+          EngineRouter.main_proxy(current_component).assignee_questionnaire_path
         end
 
         def after_update_url
