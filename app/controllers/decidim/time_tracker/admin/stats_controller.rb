ce06292cbd434e37d0262dfa0c81d994b67f9a3b
Vera Rojman <vera@platoniq.net>
2021-01-25 10:22:18 +0100

    Admin stats panel (#79)
    * Stub stats
    
    * Add filter
    
    * Fix rubocop offenses
    
    * Remove unused methods
    
    * Filter by user email
    
    * Fix breadcrumbs
    
    * Add example for stats
    
    * Add examples for filters
    
    * simplify test
    
    * do not check for ordered results
    
    Co-authored-by: Ivan Vergés <ivan@platoniq.net>

---
+++ app/controllers/decidim/time_tracker/admin/stats_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Decidim
+  module TimeTracker
+    module Admin
+      class StatsController < Admin::ApplicationController
+        include Paginable
+        include Decidim::TimeTracker::Admin::FilterableAssignations
+
+        helper Decidim::TimeTracker::ApplicationHelper
+
+        helper_method :assignations
+
+        def index; end
+
+        def assignations_collection
+          @assignations_collection ||= Decidim::TimeTracker::Assignation.joins(:task).where(decidim_time_tracker_tasks: { time_tracker_id: time_tracker.id })
+        end
+
+        alias assignations filtered_collection
+      end
+    end
+  end
+end
