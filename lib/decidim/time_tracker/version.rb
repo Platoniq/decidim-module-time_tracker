9fa569d7268188544f48f3a3d96d5e23ae5a4119
Marina Muñoz <marinamuca01@gmail.com>
2023-08-28 13:49:22 +0200

    use latest 0.26 version

--- lib/decidim/time_tracker/version.rb
+++ lib/decidim/time_tracker/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-time_tracker version.
   module TimeTracker
     VERSION = "0.4"
-    DECIDIM_VERSION = "0.26.4"
+    DECIDIM_VERSION = "0.26.8"
     COMPAT_DECIDIM_VERSION = [">= 0.25.0", "< 0.27"].freeze
   end
 end
