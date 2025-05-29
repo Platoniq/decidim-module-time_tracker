a510ffae5d0279b8f161fbf1ddbafa76834e079b
Alejandro Rueda <alejandro.rueda@nazaries.com>
2025-05-29 10:44:38 +0300

    Update to 0.29.3

--- lib/decidim/time_tracker/version.rb
+++ lib/decidim/time_tracker/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-time_tracker version.
   module TimeTracker
     VERSION = "0.7"
-    DECIDIM_VERSION = "0.29.0"
+    DECIDIM_VERSION = "0.29.3"
     COMPAT_DECIDIM_VERSION = [">= 0.29.0", "< 0.30"].freeze
   end
 end
